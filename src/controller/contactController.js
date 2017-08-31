acba38f5ae6556e771a257403845fa6c6a2c9d80
Swapnil Nakate <nakate.swapnil7@gmail.com>
2017-09-01 00:14:35 +0530

    Update contactController.js

--- src/controller/contactController.js
+++ src/controller/contactController.js
@@ -5,8 +5,8 @@ angular.module('angularApp')
     .controller('contactController', [ '$scope',  function ($scope) {
 
         $scope.contactMe = {
-            email  : 'fix20152@gmail.com',
-            github : 'https://github.com/fix2015'
+            email  : 'nakate.swapnil7@gmail.com',
+            github : 'https://github.com/swapnilnakate7'
         }
 
     }]);
